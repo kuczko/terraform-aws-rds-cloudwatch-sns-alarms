f796c619a1d7508a7666a723044b94c066a54179
Mateusz Kaminski <mateusz@kaminscy.biz>
2020-10-15 02:23:57 +0200

    Change topic variable to array

--- main.tf
+++ main.tf
@@ -62,7 +62,8 @@ data "aws_iam_policy_document" "sns_topic_policy" {
     ]
 
     effect    = "Allow"
-    resources = [local.sns_topic]
+    #resources = [local.sns_topic]
+    resources = [aws_sns_topic.default.arn]
 
     principals {
       type        = "AWS"
@@ -82,7 +83,8 @@ data "aws_iam_policy_document" "sns_topic_policy" {
   statement {
     sid       = "Allow CloudwatchEvents"
     actions   = ["sns:Publish"]
-    resources = [local.sns_topic]
+    #resources = [local.sns_topic]
+    resources = [aws_sns_topic.default.arn]
 
     principals {
       type        = "Service"
@@ -93,7 +95,7 @@ data "aws_iam_policy_document" "sns_topic_policy" {
   statement {
     sid       = "Allow RDS Event Notification"
     actions   = ["sns:Publish"]
-    resources = [local.sns_topic]
+    resources = [aws_sns_topic.default.arn]
 
     principals {
       type        = "Service"
